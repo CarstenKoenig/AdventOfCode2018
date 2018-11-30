36bf9c90bdf3218ce2cb772c4a991945b4d526ce
Carsten König <carsten@gettingsharper.de>
2018-11-30 14:15:45 +0100

    initial commit - basic setup

---
+++ src/Day1/Solution.hs
@@ -0,0 +1,17 @@
+module Day1.Solution where
+
+
+run :: IO ()
+run = putStrLn "day1 starts here"
+
+
+type Input = ()
+type Output = ()
+
+
+input :: Input
+input = ()
+
+
+solution :: Input -> Output
+solution = id
