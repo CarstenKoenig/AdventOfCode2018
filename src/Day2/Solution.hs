a7212faab37bb3a3ce9a34ceb511b7b43b41e79d
Carsten König <carsten@gettingsharper.de>
2018-12-02 08:23:42 +0100

    Day 2 both stars

--- src/Day2/Solution.hs
+++ src/Day2/Solution.hs
@@ -1,19 +1,22 @@
 module Day2.Solution where
 
-import Data.List (sort, sortBy, group)
-import Data.Ord (comparing)
 import Data.Bifunctor (bimap)
 import Data.Bool (bool)
-import Data.Monoid (Sum(..))
+import Data.List (sort, sortBy, group)
+import Data.Maybe (fromJust)
+import Data.Monoid (Sum(..), First(..))
+import Data.Ord (comparing)
 
 run :: IO ()
 run = do
   putStrLn "DAY 2"
   inp <- input
   putStrLn $ "part 1: " ++ show (part1 inp)
+  putStrLn $ "part 2: " ++ show (part2 inp)
 
 
 type BoxId = String
+
 
 input :: IO [BoxId]
 input = lines <$> readFile "./src/Day2/input.txt"
@@ -45,3 +48,27 @@ containsThreeLetters :: Grouping -> Bool
 containsThreeLetters = any (\(_,n) -> n == 3)
 
 
+----------------------------------------------------------------------
+
+part2 :: [BoxId] -> BoxId
+part2 = fromJust . findMatches
+
+
+findMatches :: [BoxId] -> Maybe BoxId
+findMatches [] = Nothing
+findMatches (a:rest) = getFirst (mconcat $ (First . isMatch a <$> rest)) <> findMatches rest
+
+
+isMatch :: BoxId -> BoxId -> Maybe BoxId
+isMatch box1 box2
+  | length com == length box1 - 1 = Just com
+  | otherwise                     = Nothing
+  where com = common box1 box2
+
+
+common :: BoxId -> BoxId -> BoxId
+common [] _ = ""
+common _ [] = ""
+common (a:as) (b:bs)
+  | a == b    = a : common as bs
+  | otherwise = common as bs
