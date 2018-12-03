8e7b7ed0a3d4cbb644b90180f08be4f27e94ab27
Carsten König <Carsten.Koenig@hotmail.de>
2018-12-03 14:28:42 +0100

    cleanup day 3

--- src/Day3/Solution.hs
+++ src/Day3/Solution.hs
@@ -6,6 +6,26 @@ import qualified Data.Map.Strict as Map
 import           Text.Parsec
 
 
+type Coord = (Int, Int)
+
+
+-- | the Fabric - a map of coordinates with counters how often the spot was marked
+type Fabric = Map Coord Int
+
+
+-- | a claim as given by the problem
+data Claim = Claim
+  { claimId :: Int
+  , left :: Int
+  , top :: Int
+  , width :: Int
+  , height :: Int
+  } deriving (Show)
+
+
+----------------------------------------------------------------------
+-- run / parts 1/2
+
 run :: IO ()
 run = do
   putStrLn "DAY 3"
@@ -13,10 +33,6 @@ run = do
   let fab = fabric claims
   putStrLn $ "part 1: " ++ show (part1 fab)
   putStrLn $ "part 2: " ++ show (part2 fab claims)
-
-
-inputTxt :: IO [Claim]
-inputTxt = map parseLine . lines <$> readFile "./src/Day3/input.txt"
 
 
 part1 :: Fabric -> Int
@@ -27,45 +43,57 @@ part2 :: Fabric -> [Claim] -> Int
 part2 fab claims =
   claimId . head $ filter (notOverlapping fab) claims
 
-type Coord = (Int, Int)
 
-type Fabric = Map Coord Int
+-----------------------------------------------------------------------
+-- problem domain
 
 
+-- | initializes a Fabric marking a list of claims
 fabric :: [Claim] -> Fabric
 fabric = foldr mark Map.empty
 
 
+-- | checks if a claim was not overlapped
+-- by validating that all points of the claim were only marked once
 notOverlapping :: Fabric -> Claim -> Bool
 notOverlapping fab claim =
-  all onceInClaim $ points claim
+  all onlyOnceInFabric $ points claim
   where
-    onceInClaim pt = Map.lookup pt fab == Just 1
+    onlyOnceInFabric pt = Map.lookup pt fab == Just 1
 
 
+-- | the area of all the overlapps is just the number of points with
+-- more than one mark
 overlappArea :: Fabric -> Int
-overlappArea = length . Map.toList . Map.filter (> 1)
+overlappArea =
+  length . Map.toList . Map.filter (> 1)
 
 
+-- | increases the mark of all points in the fabric that lie in the claims region
 mark :: Claim -> Fabric -> Fabric
-mark claim = flip (foldr markPoint) (points claim)
+mark claim =
+  flip (foldr markPoint) (points claim)
 
+
+-- | a list of all the coordinates/spots in the claim
 points :: Claim -> [Coord]
 points claim = [(left claim + x, top claim + y) | x <- [0..width claim - 1], y <- [0..height claim - 1] ]
 
 
+-- | marks a Coord in the Fabric = increases the coordinats counter
 markPoint :: Coord -> Fabric -> Fabric
 markPoint c = Map.insertWith (+) c 1
 
 
-data Claim = Claim
-  { claimId :: Int
-  , left :: Int
-  , top :: Int
-  , width :: Int
-  , height :: Int
-  } deriving (Show)
+----------------------------------------------------------------------
+-- file input
 
+inputTxt :: IO [Claim]
+inputTxt = map parseLine . lines <$> readFile "./src/Day3/input.txt"
+
+
+----------------------------------------------------------------------
+-- Parsing
 
 parseLine :: String -> Claim
 parseLine = either (error . show) id . parse lineP "input.txt"
