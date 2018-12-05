9dafe5ae7c63da2851f05a565b1807fdc6538a57
Carsten König <Carsten.Koenig@hotmail.de>
2018-12-05 06:27:58 +0100

    Day 5 part 2

--- src/Day5/Solution.hs
+++ src/Day5/Solution.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE BangPatterns #-}
 module Day5.Solution where
 
-import Data.Char (isDigit)
 import Data.Char (toUpper, isLower)
-import Text.Parsec
+import Data.List (minimumBy)
+import Data.Ord (comparing)
 
 
 type Polymer = String
@@ -13,15 +14,19 @@ run = do
   putStrLn "DAY 5"
   polymer <- inputTxt
   let afterReaction = fullReaction polymer
-  putStrLn $ "part 1: " ++ show (length afterReaction)
+  let initialLenght = length afterReaction
+  putStrLn $ "part 1: " ++ show initialLenght
+  let best = findBest afterReaction
+  putStrLn $ "part 2: " ++ show (length best)
 
 
-inputTxt :: IO Polymer
-inputTxt = filter (`elem` allowed) <$> readFile "./src/Day5/input.txt"
+improve :: Char -> Polymer -> Polymer
+improve p = fullReaction . filter (\c -> c /= p && c /= toUpper p)
 
 
-allowed :: String
-allowed = ['a'..'z'] ++ ['A'..'Z']
+findBest :: Polymer -> Polymer
+findBest start =
+  minimumBy (comparing length) [ improve c start | c <- ['a' .. 'z' ] ]
 
 
 fullReaction :: Polymer -> Polymer
@@ -39,24 +44,16 @@ canReact :: Char -> Char -> Bool
 canReact a a' =
   toUpper a == toUpper a' && isLower a /= isLower a'
 
+
+inputTxt :: IO Polymer
+inputTxt = filter (`elem` allowed) <$> readFile "./src/Day5/input.txt"
+
+
+allowed :: String
+allowed = ['a'..'z'] ++ ['A'..'Z']
+
 fix :: Eq a => (a -> a) -> a -> a
-fix f x =
+fix f !x =
   let x' = f x
   in if x' == x then x else fix f x'
 
-
-test :: Polymer
-test = "dabAcCaCBAcCcaDA"
-
--- parseInput :: String -> Input
--- parseInput = either (error . show) id . parse inputP "input.txt"
-
-
--- type Parser a = Parsec String () a
-
-
--- inputP :: Parser Input
--- inputP = pure ()
---   where
---     intP :: Parser Int
---     intP = read <$> many1 (satisfy isDigit)
