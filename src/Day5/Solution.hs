8b2a732af7616f1b0ce6debc00a4557c92151acd
Carsten König <Carsten.Koenig@hotmail.de>
2018-12-05 06:16:04 +0100

    Day 5 part 1

--- src/Day5/Solution.hs
+++ src/Day5/Solution.hs
@@ -1,29 +1,62 @@
 module Day5.Solution where
 
-import           Data.Char (isDigit)
-import           Text.Parsec
+import Data.Char (isDigit)
+import Data.Char (toUpper, isLower)
+import Text.Parsec
+
+
+type Polymer = String
 
 
 run :: IO ()
 run = do
   putStrLn "DAY 5"
+  polymer <- inputTxt
+  let afterReaction = fullReaction polymer
+  putStrLn $ "part 1: " ++ show (length afterReaction)
 
 
-inputTxt :: IO String
-inputTxt = readFile "./src/Day5/input.txt"
+inputTxt :: IO Polymer
+inputTxt = filter (`elem` allowed) <$> readFile "./src/Day5/input.txt"
 
 
-type Input = ()
-
-parseInput :: String -> Input
-parseInput = either (error . show) id . parse inputP "input.txt"
+allowed :: String
+allowed = ['a'..'z'] ++ ['A'..'Z']
 
 
-type Parser a = Parsec String () a
+fullReaction :: Polymer -> Polymer
+fullReaction = fix react
 
 
-inputP :: Parser Input
-inputP = pure ()
-  where
-    intP :: Parser Int
-    intP = read <$> many1 (satisfy isDigit)
+react :: Polymer -> Polymer
+react (a:b'@(b:rest))
+  | canReact a b = react rest
+  | otherwise    = a : react b'
+react xs = xs
+
+
+canReact :: Char -> Char -> Bool
+canReact a a' =
+  toUpper a == toUpper a' && isLower a /= isLower a'
+
+fix :: Eq a => (a -> a) -> a -> a
+fix f x =
+  let x' = f x
+  in if x' == x then x else fix f x'
+
+
+test :: Polymer
+test = "dabAcCaCBAcCcaDA"
+
+-- parseInput :: String -> Input
+-- parseInput = either (error . show) id . parse inputP "input.txt"
+
+
+-- type Parser a = Parsec String () a
+
+
+-- inputP :: Parser Input
+-- inputP = pure ()
+--   where
+--     intP :: Parser Int
+--     intP = read <$> many1 (satisfy isDigit)
