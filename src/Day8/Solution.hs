64faddb892842d7319e39a929b4bc1856c7a2386
Carsten König <carsten@gettingsharper.de>
2018-12-08 08:42:46 +0100

    Day 8 part 1

--- src/Day8/Solution.hs
+++ src/Day8/Solution.hs
@@ -9,21 +9,51 @@ run = do
   putStrLn "DAY 8"
 
 
-inputTxt :: IO String
-inputTxt = readFile "./src/Day8/input.txt"
+type Input = [Number]
+type Number = Int
 
 
-type Input = ()
+data Node = Node
+  { nrChildren :: Int
+  , nrMetadata :: Int
+  , children   :: [Node]
+  , metadata   :: [Number]
+  } deriving Show
 
-parseInput :: String -> Input
-parseInput = either (error . show) id . parse inputP "input.txt"
+
+checkSum :: Node -> Int
+checkSum (Node _ _ chs meta) =
+  sum meta + sum (map checkSum chs)
+
+
+parseInput :: String -> Node
+parseInput = either (error . show) id . parse nodeP "input.txt"
 
 
 type Parser a = Parsec String () a
 
 
-inputP :: Parser Input
-inputP = pure ()
-  where
-    intP :: Parser Int
-    intP = read <$> many1 (satisfy isDigit)
+nodeP :: Parser Node
+nodeP = do
+  nrCh <- numberP
+  nrMeta <- numberP
+  child <- count nrCh nodeP
+  meta <- count nrMeta numberP
+  return $ Node nrCh nrMeta child meta
+
+
+numberP :: Parser Number
+numberP = read <$> many1 (satisfy isDigit) <* spaces
+
+
+example :: String
+example = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+
+
+inputExample :: Node
+inputExample = parseInput example
+
+
+inputTxt :: IO Node
+inputTxt = parseInput <$> readFile "./src/Day8/input.txt"
+
