2b54fc62552bf895da7ef358b26ffc181558c818
Carsten König <carsten@gettingsharper.de>
2018-12-08 09:05:18 +0100

    cleaned up Day 8

--- src/Day8/Solution.hs
+++ src/Day8/Solution.hs
@@ -3,6 +3,7 @@ module Day8.Solution where
 import           Data.Char (isDigit)
 import           Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
+import           Data.Monoid (Sum(..))
 import           Text.Parsec
 
 
@@ -11,54 +12,77 @@ run = do
   putStrLn "DAY 8"
 
   node <- inputTxt
-  putStrLn $ "part 1: " ++ show (checkSum node)
-  putStrLn $ "part 2: " ++ show (value node)
+  putStrLn $ "part 1: " ++ show (getSum $ checkSum node)
+  putStrLn $ "part 2: " ++ show (getSum $ value node)
 
 
-type Input = [Number]
+-- | the input is a list of numbers
 type Number = Int
 
 
+-- | the problem represents a tree of nodes
+-- each node has children and meta-data (numbers)
 data Node = Node
-  { nrChildren :: Int
-  , nrMetadata :: Int
-  , children   :: IntMap Node
+  { children   :: IntMap Node
   , metadata   :: [Number]
   } deriving Show
 
 
-checkSum :: Node -> Int
-checkSum (Node _ _ chs meta) =
-  sum meta + sum (map checkSum $ IntMap.elems chs)
+-- | part 1:
+-- the checksum is just the sum of metadata + the sum of checksum of all children
+checkSum :: Node -> Sum Int
+checkSum (Node chs meta) =
+  foldMap Sum meta <> foldMap checkSum chs
 
 
-value :: Node -> Int
-value (Node 0 _ _ meta) = sum meta
-value (Node _ _ chds ids) =
-  sum $ map getChildValue ids
+-- | part 2:
+-- if a node has no Children the value is just the sum of the metadata
+-- if a node has Children the metadata represent indizes and the value
+-- of the node becomes the sum of values of existing children
+value :: Node -> Sum Int
+value (Node chds meta) | null chds =
+  foldMap Sum meta
+value (Node chds ids) =
+  foldMap getChildValue ids
   where
     getChildValue ind =
       maybe 0 value $ IntMap.lookup ind chds
 
-parseInput :: String -> Node
-parseInput = either (error . show) id . parse nodeP "input.txt"
 
+----------------------------------------------------------------------
+-- Parsing
 
 type Parser a = Parsec String () a
 
 
+-- | parses a stream of numbers into nodes
 nodeP :: Parser Node
 nodeP = do
   nrCh <- numberP
   nrMeta <- numberP
   child <- IntMap.fromList . zip [1..] <$> count nrCh nodeP
   meta <- count nrMeta numberP
-  return $ Node nrCh nrMeta child meta
+  return $ Node child meta
 
 
+-- | parses a number suffixed by spaces
 numberP :: Parser Number
 numberP = read <$> many1 (satisfy isDigit) <* spaces
 
+
+----------------------------------------------------------------------
+-- IO
+
+parseInput :: String -> Node
+parseInput = either (error . show) id . parse nodeP "input.txt"
+
+
+inputTxt :: IO Node
+inputTxt = parseInput <$> readFile "./src/Day8/input.txt"
+
+
+----------------------------------------------------------------------
+-- example data
 
 example :: String
 example = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
@@ -66,8 +90,3 @@ example = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
 
 inputExample :: Node
 inputExample = parseInput example
-
-
-inputTxt :: IO Node
-inputTxt = parseInput <$> readFile "./src/Day8/input.txt"
-
