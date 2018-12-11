c4923691eb6b1cb4b44146e9547c7eb7e5c10a9f
Carsten König <Carsten.Koenig@hotmail.de>
2018-12-11 14:15:34 +0100

    optimized and cleaned up quite a bit - summed-area tables for the win

--- src/Day11/Solution.hs
+++ src/Day11/Solution.hs
@@ -1,12 +1,15 @@
+-- | sollution of Day 11 using a [summed-area table](https://en.wikipedia.org/wiki/Summed-area_table) approach
+
 module Day11.Solution (run) where
 
 import           Data.Array (Array, (!))
 import qualified Data.Array as A
 import           Data.Ix (range)
-import           Data.List (maximumBy, scanl')
+import           Data.List (maximumBy)
 import           Data.Ord (comparing)
 
-
+-- | for each coord (x,y) this table holds the sum of
+-- the arrea in a grid starting at (1,1) to (x,y)
 type Grid = Array Coord PowerLevel
 
 type Coord = (Int, Int)
@@ -15,56 +18,55 @@ type Size = Int
 type GridSerial = Int
 
 
-findMaxSquareCoord :: GridSerial -> Size -> (Coord, Size)
-findMaxSquareCoord gS maxSz = (\(c,sz,_) -> (c,sz)) $
-  maximumBy (comparing (\(_,_,pl) -> pl)) . concat $
-  [ partialTotalPower grid (c, min maxSz (301 - max x y)) | c@(x,y) <- coords ]
-  where
-    grid = powerGrid gS
-    coords = coordsInGrid grid
+run :: IO ()
+run = do
+  putStrLn "DAY 11"
+  let grid = powerGrid gridSerial
+  putStrLn $ "part 1: " ++ show (findMax 3 grid)
+  putStrLn $ "part 2: " ++ show (findMax 300 grid)
 
 
-partialTotalPower :: Grid -> (Coord, Size) -> [(Coord, Size, PowerLevel)]
-partialTotalPower grid (c,sz) =
-  tail $ scanl' addPartial (c, 0, 0) (partialSquareCoords sz c)
-  where
-    addPartial (_, _, total) (cs, s) =
-      (c, s, total + sum (powerLevel grid <$> cs))
+-- | this is my problem number for today - yours might differ
+gridSerial :: GridSerial
+gridSerial = 4842
 
 
-partialSquareCoords :: Size -> Coord -> [([Coord], Size)]
-partialSquareCoords sz (cx,cy) = [ (border i, i) | i <- [1..sz] ]
-  where
-    border 1 = [(cx,cy)]
-    border n = [ (cx+n-1,y) | y <- [cy..cy+n-2] ] ++ (cx+n-1,cy+n-1) :  [(x,cy+n-1) | x <- [cx+n-2,cx+n-3..cx] ]
+-- | enumerate all valid areas in the grid and find the one
+-- with maximum power-level sum using the summed-area table
+findMax :: Size -> Grid -> (Coord, Size)
+findMax maxSize grid =
+  maximumBy (comparing (\(c@(x,y), sz) -> areaPowerLevel grid (c,(x+sz,y+sz)))) $
+  [ (c, sz) | c@(x,y) <- A.indices grid, sz <- [1..min maxSize (301 - max x y) ] ]
 
 
-powerLevel :: Grid -> Coord -> PowerLevel
-powerLevel = (!)
+-- | calcualtes the summed power-level using a summed-area-table
+-- see [Wikipedia](https://en.wikipedia.org/wiki/Summed-area_table#The_algorithm)
+-- ![Sum = D - B - C + A](https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Summed_area_table.png/220px-Summed_area_table.png)
+areaPowerLevel :: Grid -> (Coord, Coord) -> PowerLevel
+areaPowerLevel grid (c@(x,y),c'@(x',y'))
+  | A.inRange gridBounds c && A.inRange gridBounds c' = grid!c' + grid!c - grid!(x',y) - grid!(x,y')
+  | otherwise = 0
 
 
+-- | calculates a summed-area-table from the cached power-level values
 powerGrid :: GridSerial -> Grid
-powerGrid gS = A.array gridBounds [ (c, getLevel c) | c <- coords ]
+powerGrid gS =
+  let arr = A.array gridBounds [(c, sumUL c) | c <- gridCoords ]
+      sumUL (1,1)   = powerLevel (1,1)
+      sumUL (!x,1)  = powerLevel (x,1) + sumUL (x-1,1)
+      sumUL (1,!y)  = powerLevel (1,y) + sumUL (1,y-1)
+      sumUL (!x,!y) = powerLevel (x,y) + arr ! (x,y-1) + arr ! (x-1,y) - arr ! (x-1,y-1)
+  in arr
   where
-    gridBounds = ((1,1), (300,300))
-    coords     = range gridBounds
-    getLevel (x,y) =
+    powerLevel (x,y) =
       hundredsDigit ((rackId * y + gS) * rackId) - 5
       where rackId   = x + 10
     hundredsDigit n  = (n `mod` 1000) `div` 100
 
 
-coordsInGrid :: Grid -> [Coord]
-coordsInGrid = A.indices
+gridBounds :: (Coord, Coord)
+gridBounds = ((1,1), (300,300))
 
 
-run :: IO ()
-run = do
-  putStrLn "DAY 11"
-
-  putStrLn $ "part 1: " ++ show (findMaxSquareCoord gridSerial 3)
-  putStrLn $ "part 2: " ++ show (findMaxSquareCoord gridSerial 10)
-
-
-gridSerial :: GridSerial
-gridSerial = 4842
+gridCoords :: [Coord]
+gridCoords = range gridBounds
--- src/Day11/readme.md
+++ src/Day11/readme.md
@@ -5,3 +5,8 @@ the puzzle for today can be found [here](https://adventofcode.com/2018/day/11).
 todays problem asks us to find a maximum square of powerlevels
 
 part 2 has some intersting options to optimize the calculation
+
+---
+
+UPDATE: I missed a rather obvious optimization technique - using a [Summed-area table](https://en.wikipedia.org/wiki/Summed-area_table)
+approach brings back the time to 1/10th - which is fine for me as it's under my 5s goal.
