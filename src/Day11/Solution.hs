ec1b4a061e062106fd74f6676153ba04162b6694
Carsten König <Carsten.Koenig@hotmail.de>
2018-12-11 09:20:38 +0100

    day 11 part2 in around 22s - not good but better

--- src/Day11/Solution.hs
+++ src/Day11/Solution.hs
@@ -1,9 +1,9 @@
-module Day11.Solution where
+module Day11.Solution (run) where
 
 import           Data.Array (Array, (!))
 import qualified Data.Array as A
-import           Data.List (maximumBy)
-import Data.Ord (comparing)
+import           Data.List (maximumBy, scanl')
+import           Data.Ord (comparing)
 
 
 type Grid = Array Coord PowerLevel
@@ -13,22 +13,25 @@ type PowerLevel = Int
 
 
 findMaxSquareCoord :: Int -> (Coord, Int)
-findMaxSquareCoord gS = maximumBy (comparing (totalPower grid)) $ [ c | i <- [1..300], c <- allSquareCoords i ]
+findMaxSquareCoord gS =
+  (\(c,sz,_) -> (c,sz)) $ maximumBy (comparing (\(_,_,pl) -> pl)) . concat $ [ partialTotalPower grid (c, 301 - max x y) | c@(x,y) <- coords ]
   where grid = powerGrid gS
 
 
 
-findMaxSquareCoord' :: Int -> Int -> (Coord, Int)
-findMaxSquareCoord' gS sz = maximumBy (comparing (totalPower grid)) $ allSquareCoords sz
-  where grid = powerGrid gS
+partialTotalPower :: Grid -> (Coord, Int) -> [(Coord, Int, PowerLevel)]
+partialTotalPower grid (c,sz) =
+  tail $ scanl' addPartial (c, 0, 0) (partialSquareCoords sz c)
+  where
+    addPartial :: (Coord, Int, PowerLevel) -> ([Coord], Int) -> (Coord, Int, PowerLevel)
+    addPartial (_, _, total) (cs, s) = (c, s, total + sum ((grid !) <$> cs))
 
 
-allSquareCoords :: Int -> [(Coord,Int)]
-allSquareCoords size = [ ((x,y), size) | x <- [1..301-size], y <- [1..301-size] ]
-
-
-totalPower :: Grid -> (Coord, Int) -> PowerLevel
-totalPower grid sqCoord = sum . map (grid !) $ squareCoords sqCoord
+partialSquareCoords :: Int -> Coord -> [([Coord], Int)]
+partialSquareCoords sz (cx,cy) = [ (border i, i) | i <- [1..sz] ]
+  where
+    border 1 = [(cx,cy)]
+    border n = [ (cx+n-1,y) | y <- [cy..cy+n-2] ] ++ (cx+n-1,cy+n-1) :  [(x,cy+n-1) | x <- [cx+n-2,cx+n-3..cx] ]
 
 
 gridBounds :: (Coord, Coord)
@@ -39,21 +42,12 @@ coords :: [Coord]
 coords = [ (x,y) | y <- [1..300], x <- [1..300]]
 
 
-squareCoords :: (Coord,Int) -> [Coord]
-squareCoords ((x,y), sz) =
-  [ (x+dx, y+dy) | dx <- [0..sz-1], dy <- [0..sz-1] ]
-
-
 powerGrid :: Int -> Grid
-powerGrid gS = A.array gridBounds [ (c, powerLevel' gS c) | c <- coords ]
+powerGrid gS = A.array gridBounds [ (c, powerLevel gS c) | c <- coords ]
 
 
-powerLevel :: Coord -> PowerLevel
-powerLevel = powerLevel' gridSerial
-
-
-powerLevel' :: Int -> Coord -> PowerLevel
-powerLevel' gS (x,y) =
+powerLevel :: Int -> Coord -> PowerLevel
+powerLevel gS (x,y) =
   hundredsDigit ((rackId * y + gS) * rackId) - 5
   where
     hundredsDigit n = (n `mod` 1000) `div` 100
@@ -64,7 +58,8 @@ run :: IO ()
 run = do
   putStrLn "DAY 11"
 
-  putStrLn $ "part 1: " ++ show (findMaxSquareCoord gridSerial)
+  putStrLn $ "part 2: " ++ show (findMaxSquareCoord gridSerial)
+
 
 gridSerial :: Int
 gridSerial = 4842
