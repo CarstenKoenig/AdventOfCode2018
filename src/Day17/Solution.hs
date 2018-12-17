cf765b952e5ab705671fcd2b7019012e1b5e2a32
Carsten König <Carsten.Koenig@hotmail.de>
2018-12-17 06:27:22 +0100

    Day 17 - parse input

--- src/Day17/Solution.hs
+++ src/Day17/Solution.hs
@@ -1,6 +1,10 @@
 module Day17.Solution where
 
 import           Data.Char (isDigit)
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import           Data.Set (Set)
+import qualified Data.Set as S
 import           Text.Parsec
 
 
@@ -9,21 +13,49 @@ run = do
   putStrLn "DAY 17"
 
 
-inputTxt :: IO String
-inputTxt = readFile "./src/Day17/input.txt"
+inputTxt :: IO ClayCoords
+inputTxt = parseInput <$> readFile "./src/Day17/input.txt"
 
 
-type Input = ()
-
-parseInput :: String -> Input
+parseInput :: String -> ClayCoords
 parseInput = either (error . show) id . parse inputP "input.txt"
 
+parse' :: Parser a -> String -> a
+parse' p = either (error . show) id . parse p "input.txt"
 
 type Parser a = Parsec String () a
 
 
-inputP :: Parser Input
-inputP = pure ()
+inputP :: Parser ClayCoords
+inputP = S.fromList . concat <$> (many $ (coordP <|> coordP') <* many newline)
+
+
+coordP :: Parser [Coord]
+coordP = do
+  xs <- coordCompP 'x'
+  _ <- string ", "
+  ys <- coordCompP 'y'
+  pure $ [ (x,y) | x <- xs, y <- ys ]
+
+
+coordP' :: Parser [Coord]
+coordP' = do
+  ys <- coordCompP 'y'
+  _ <- string ", "
+  xs <- coordCompP 'x'
+  pure $ [ (x,y) | x <- xs, y <- ys ]
+
+
+coordCompP :: Char -> Parser [Int]
+coordCompP c = char c *> char '=' *> rangeP
+
+
+rangeP :: Parser [Int]
+rangeP = do
+  start <- numP
+  maybe [start] (\end -> [start..end]) <$> (optionMaybe $ do
+    _ <- string ".."
+    numP)
 
 
 intP :: Parser Int
@@ -32,3 +64,18 @@ intP = choice [ negate <$> (char '-' *> numP), numP ]
 
 numP :: Parser Int
 numP = read <$> many1 (satisfy isDigit)
+
+
+type Coord = (Int, Int)
+
+type World = Map Coord Content
+
+data Content
+  = Sand
+  | Clay
+  | DrySand
+  | Water
+  deriving Show
+
+
+type ClayCoords = Set Coord
