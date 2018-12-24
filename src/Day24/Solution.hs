bf1f2393e45466bf0eac96afb8f2d070a5e406d8
Carsten König <carsten@gettingsharper.de>
2018-12-24 08:51:57 +0100

    day 24 part 1

--- src/Day24/Solution.hs
+++ src/Day24/Solution.hs
@@ -2,11 +2,16 @@
 {-# LANGUAGE TupleSections #-}
 module Day24.Solution where
 
-import Data.Char (isDigit)
-import Data.List (groupBy, sortBy)
-import Data.Ord (comparing)
-import Text.Parsec
-import Data.Function (on)
+import           Control.Arrow ((&&&))
+import           Data.Char (isDigit)
+import           Data.Function (on)
+import           Data.List (groupBy, sortBy, maximumBy, foldl')
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Ord (Down(..), comparing)
+import           Data.Set (Set)
+import qualified Data.Set as Set
+import           Text.Parsec hiding (State)
 
 
 type HitPoints = Int
@@ -27,12 +32,133 @@ data Group = Group
   , weaknesses    :: [AttackType]
   , immunites     :: [AttackType]
   , initiative    :: Initiative
-  } deriving Show
+  } deriving (Eq, Show)
+
+
+type GroupNr = Int
+
+data State = State
+  { groups        :: Map GroupNr Group
+  , immuneArmy    :: Set GroupNr
+  , infectionArmy :: Set GroupNr
+  } deriving (Eq, Show)
+
+
+part1 :: Input -> Int
+part1 inp =
+  let state = initState inp
+      end = simulate state
+  in sum . map groupUnits . Map.elems $ groups end
+
+
+initState :: (Army, Army) -> State
+initState (imms, infs) =
+  let immNrs = Set.fromList $ take (length imms) [1..]
+      infNrs = Set.fromList $ take (length infs) [length imms+1..]
+      grps   = Map.fromList $ zip [1..] (imms ++ infs)
+  in State grps immNrs infNrs
+
+
+simulate :: State -> State
+simulate state =
+  let state' = oneRound state
+  in if state == state' then state else simulate state'
+
+
+oneRound :: State -> State
+oneRound state =
+  let sels = selectionPhase state
+      afterAtk = attackPhase sels state
+  in removeDead afterAtk
+
+
+removeDead :: State -> State
+removeDead state =
+  let groups' = Map.filter (\g -> groupUnits g > 0) $ groups state
+      immArmy = Set.filter (\nr -> nr `Map.member` groups') $ immuneArmy state
+      infArmy = Set.filter (\nr -> nr `Map.member` groups') $ infectionArmy state
+  in State groups' immArmy infArmy
+
+
+attackPhase :: [(GroupNr, Maybe GroupNr)] -> State -> State
+attackPhase sels state =
+  foldl' (flip attack) state sels
+
+
+attack :: (GroupNr, Maybe GroupNr) -> State -> State
+attack (_, Nothing) state = state
+attack (attNr, Just defNr) state =
+  let att = getGroup state attNr
+      def = getGroup state defNr
+      dmg = calcDamage att def
+      killed = dmg `div` unitHitpoints def
+  in state { groups = Map.insert defNr (def { groupUnits = max 0 (groupUnits def - killed)}) (groups state) }
+
+
+getGroup :: State -> GroupNr -> Group
+getGroup state = (groups state Map.!)
+
+
+selectionPhase :: State -> [(GroupNr, Maybe GroupNr)]
+selectionPhase state =
+  let selections =
+        targetSelection state (immuneArmy state) (infectionArmy state)
+        ++ targetSelection state (infectionArmy state) (immuneArmy state)
+  in sortBy (comparing (negate . initiative . getGroup state . fst)) selections
+
+
+targetSelection :: State -> Set GroupNr -> Set GroupNr -> [(GroupNr, Maybe GroupNr)]
+targetSelection state attackers =
+  go (sortBy (comparing (Down . order . snd)) $ map (id &&& getGroup state) $ Set.toList attackers)
+  where
+    go :: [(GroupNr, Group)] -> Set GroupNr -> [(GroupNr, Maybe GroupNr)]
+    go [] _ = []
+    go ((attNr, att):rest) defs
+      | Set.null defs = []
+      | otherwise =
+        let (d,remDefs) = highestDmgPotential state att defs
+        in (attNr, d) : go rest remDefs
+    order gr = (effectivePower gr, initiative gr)
+
+
+highestDmgPotential :: State -> Group -> Set GroupNr -> (Maybe GroupNr, Set GroupNr)
+highestDmgPotential state att defNrs =
+  let target = maximumBy (comparing (order . getGroup state)) $ Set.toList defNrs
+  in
+    if calcDamage att (getGroup state target) <= 0
+    then (Nothing, defNrs)
+    else (Just target, Set.delete target defNrs)
+  where
+    order gr = (calcDamage att gr, effectivePower gr, initiative gr)
+
+
+calcDamage :: Group -> Group -> HitPoints
+calcDamage att def =
+  let factor = calcModifier (attackType att)
+  in effectivePower att * factor
+  where
+    calcModifier attType =
+      if attType `elem` immunites def
+      then 0
+      else if attType `elem` weaknesses def
+      then 2
+      else 1
+
+
+effectivePower :: Group -> AttackDamage
+effectivePower gr = groupUnits gr * attackDamage gr
 
 
 run :: IO ()
 run = do
   putStrLn "DAY 24"
+
+  inp <- inputTxt
+  putStrLn $ "part 1: " ++ show (part1 inp)
+
+
+exampleTxt :: IO Input
+exampleTxt = parseInput <$> readFile "./src/Day24/example.txt"
 
 
 inputTxt :: IO Input
